964067bffa0805d5dc8aa9fffd6da5cd7786d8f8
rhc921004@gmail.com <rhc921004@gmail.com>
2022-02-25 23:31:18 -0400

    chore: removing autogenerate file build

--- build/config/jwt.secret.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable */
-exports.default = {
-    jwtSecret: "@QEGTUI",
-};
-//# sourceMappingURL=jwt.secret.js.map
--- build/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* eslint-disable */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = exports.User = void 0;
-const user_1 = require("./user");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_1.User; } });
-const role_1 = require("./role");
-Object.defineProperty(exports, "Role", { enumerable: true, get: function () { return role_1.Role; } });
-//# sourceMappingURL=index.js.map
